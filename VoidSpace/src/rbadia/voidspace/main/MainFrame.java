d461de776a00eb49581463e4a65e8b9c333ae1ff
Albert Morales <albert.morales@upr.edu>
2016-04-28 17:21:48 -0400

    Boss shot sound

--- VoidSpace/src/rbadia/voidspace/main/MainFrame.java
+++ VoidSpace/src/rbadia/voidspace/main/MainFrame.java
@@ -20,24 +20,24 @@ public class MainFrame extends JFrame {
 	private JPanel jContentPane = null;
 
 	private GameScreen gameScreen = null;
-	
+
 	private JLabel destroyedLabel;
 	private JLabel destroyedValueLabel;
 
 	private JLabel shipsLabel;
 	private JLabel shipsValueLabel;
-	
+
 	private JLabel scorePointsLabel;
 	private JLabel scorePointsValueLabel;
-	
+
 	private JLabel destroyedEnemyShipsLabel;
 	private JLabel destroyedEnemyShipsValueLabel;
-	
+
 	private JLabel levelLabel;
 	private JLabel levelValueLabel;
-	
+
 	private JLabel turboMessage = new JLabel ("Press SHIFT for turbo");
-	
+
 	/**
 	 * This is the default constructor
 	 */
@@ -52,16 +52,16 @@ public class MainFrame extends JFrame {
 	 * @return void
 	 */
 	private void initialize() {
-		this.setSize(550, 490);
+		this.setSize(520, 500);
 		this.setContentPane(getJContentPane());
 		this.setTitle("BattleStar X");
-//		this.setResizable(false);
-		
+		//		this.setResizable(false);
+
 		Dimension dim = this.getToolkit().getScreenSize();
 		Rectangle bounds = this.getBounds();
 		this.setLocation(
-			(dim.width - bounds.width) / 2,
-			(dim.height - bounds.height) / 2);
+				(dim.width - bounds.width) / 2,
+				(dim.height - bounds.height) / 2);
 	}
 
 	/**
@@ -71,98 +71,98 @@ public class MainFrame extends JFrame {
 	 */
 	private JPanel getJContentPane() {
 		if (jContentPane == null) {
-			
+
 			GridBagConstraints gridBagConstraints12 = new GridBagConstraints();
 			gridBagConstraints12.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints12.gridy = 5;
 			gridBagConstraints12.anchor = GridBagConstraints.EAST;
 			gridBagConstraints12.weightx = 1.0D;
 			gridBagConstraints12.gridx = 0;
-			
+
 			GridBagConstraints gridBagConstraints11 = new GridBagConstraints();//
 			gridBagConstraints11.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints11.gridy = 4;
 			gridBagConstraints11.anchor = GridBagConstraints.EAST;
 			gridBagConstraints11.weightx = 1.0D;
 			gridBagConstraints11.gridx = 0;
-			
+
 			GridBagConstraints gridBagConstraints10 = new GridBagConstraints();
 			gridBagConstraints10.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints10.gridy = 2;
 			gridBagConstraints10.anchor = GridBagConstraints.WEST;
 			gridBagConstraints10.weightx = 1.0D;
 			gridBagConstraints10.gridx = 3;
-			
+
 			GridBagConstraints gridBagConstraints9 = new GridBagConstraints();
 			gridBagConstraints9.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints9.gridy = 2;
 			gridBagConstraints9.anchor = GridBagConstraints.EAST;
 			gridBagConstraints9.weightx = 1.0D;
 			gridBagConstraints9.gridx = 2;
-		
+
 			GridBagConstraints gridBagConstraints8 = new GridBagConstraints();
 			gridBagConstraints8.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints8.gridy = 3;
 			gridBagConstraints8.anchor = GridBagConstraints.EAST;
 			gridBagConstraints8.weightx = 1.0D;
 			gridBagConstraints8.gridx = 0;
-			
+
 			GridBagConstraints gridBagConstraints7 = new GridBagConstraints();
 			gridBagConstraints7.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints7.gridy = 3;
 			gridBagConstraints7.anchor = GridBagConstraints.WEST;
 			gridBagConstraints7.weightx = 1.0D;
 			gridBagConstraints7.gridx = 1;
-			
+
 			GridBagConstraints gridBagConstraints6 = new GridBagConstraints();
 			gridBagConstraints6.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints6.gridy = 2;
 			gridBagConstraints6.anchor = GridBagConstraints.WEST;
 			gridBagConstraints6.weightx = 1.0D;
 			gridBagConstraints6.gridx = 1;
-			
+
 			GridBagConstraints gridBagConstraints5 = new GridBagConstraints();
 			gridBagConstraints5.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints5.gridy = 2;
 			gridBagConstraints5.anchor = GridBagConstraints.EAST;
 			gridBagConstraints5.weightx = 1.0D;
 			gridBagConstraints5.gridx = 0;
-			
+
 			GridBagConstraints gridBagConstraints4 = new GridBagConstraints();
 			gridBagConstraints4.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints4.gridy = 1;
 			gridBagConstraints4.anchor = GridBagConstraints.WEST;
 			gridBagConstraints4.weightx = 1.0D;
 			gridBagConstraints4.gridx = 3;
-			
+
 			GridBagConstraints gridBagConstraints3 = new GridBagConstraints();
 			gridBagConstraints3.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints3.gridy = 1;
 			gridBagConstraints3.anchor = GridBagConstraints.EAST;
 			gridBagConstraints3.weightx = 1.0D;
 			gridBagConstraints3.gridx = 2;
-			
+
 			GridBagConstraints gridBagConstraints2 = new GridBagConstraints();
 			gridBagConstraints2.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints2.gridy = 1;
 			gridBagConstraints2.anchor = GridBagConstraints.WEST;
 			gridBagConstraints2.weightx = 1.0D;
 			gridBagConstraints2.gridx = 1;
-			
+
 			GridBagConstraints gridBagConstraints1 = new GridBagConstraints();
 			gridBagConstraints1.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints1.gridy = 1;
 			gridBagConstraints1.anchor = GridBagConstraints.EAST;
 			gridBagConstraints1.weightx = 1.0D;
 			gridBagConstraints1.gridx = 0;
-			
+
 			GridBagConstraints gridBagConstraints = new GridBagConstraints();
 			gridBagConstraints.insets = new Insets(0, 0, 0, 0);
 			gridBagConstraints.gridx = 0;
 			gridBagConstraints.gridy = 0;
 			gridBagConstraints.fill = GridBagConstraints.NONE;
 			gridBagConstraints.gridwidth = 4;
-			
+
 			shipsLabel = new JLabel("Ships Left: ");
 			shipsValueLabel = new JLabel("9");
 			destroyedLabel = new JLabel("Asteroids Destroyed: ");
@@ -173,9 +173,9 @@ public class MainFrame extends JFrame {
 			levelValueLabel = new JLabel("1 / 9");
 			scorePointsLabel = new JLabel("Total Score: ");
 			scorePointsValueLabel = new JLabel("0");
-			
+
 			turboMessage.setForeground(Color.RED);
-			
+
 			jContentPane = new JPanel();
 			jContentPane.setLayout(new GridBagLayout());
 			jContentPane.add(getGameScreen(), gridBagConstraints);
